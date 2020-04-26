4d3d46aa72a0279cf86e1850470846e02aa6083a
margaret mwangi <margaret13mwangi@gmail.com>
2020-04-26 14:04:46 +0300

    added the new files

---
+++ config.py
@@ -0,0 +1,36 @@
+import os
+
+class Config:
+
+   	NEWS_SOURCES_BASE_URL ='https://newsapi.org/v2/sources?language=en&category={}&apiKey={}'
+   	ARTICLES_BASE_URL = 'https://newsapi.org/v2/everything?language=en&sources={}&apiKey={}'
+   	NEWS_API_KEY = os.environ.get('NEWS_API_KEY')
+   	@staticmethod
+   	def init_app(app):
+   		pass
+
+class ProdConfig(Config):
+    '''
+    Production configuration child class
+    
+    Args:
+        Config: The parent configuration class with General configuration settings
+    '''
+    pass
+
+class DevConfig(Config):
+    
+    '''
+    Development configuration child class
+    
+    Args:
+        Config: The parent configuration class with General configuration settings
+    '''
+    
+    DEBUG = True
+
+
+config_options = {
+    'development': DevConfig,
+    'production': ProdConfig
+}
